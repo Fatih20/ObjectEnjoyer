b11d5a43e24512d61e48e4b0fa18ef8b356efdd3
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-02-27 01:30:04 +0700

    feat: AbilityCard class (subject to change)

---
+++ src/Classes/AbilityCard/AbilityCard.hpp
@@ -0,0 +1,53 @@
+#ifndef _ABILITYCARD_HPP_
+#define _ABILITYCARD_HPP_
+
+#include "Classes/Card/Card.hpp"
+#include <string>
+
+typedef enum{
+    REROLL, // Reroll all hand cards
+    QUADRUPLE, // Quadruple the reward of the current round
+    QUARTER, // Quarter the reward of the current round
+    REVERSE, // Reverse the order of the players
+    SWAP, // Swap a card from the hand with a card from another player's hand
+    SWITCH, // Switch the hand cards with another player's hand cards
+    ABILITYLESS // Disable the ability of a specific player
+} Ability;
+
+// bagaimana memasukkan enumeration ke dalam template?
+
+class AbilityCard: public Card<Ability> {
+    private:
+        std::string description;
+    
+    public:
+        /**
+         * @brief Construct a new Ability Card object with default value NULL
+         * 
+         */
+        AbilityCard();
+
+        /**
+         * @brief Construct a new Ability Card object with given ability
+         * 
+         * @param value The ability of the card (REROLL, QUADRUPLE, QUARTER, REVERSE, SWAP, SWITCH, ABILITYLESS)
+         */
+        AbilityCard(Ability value, std::string description);
+        // CCtor dan assignment operator tidak perlu karena tidak ada pointer
+        // Dtor tidak perlu karena hanya menyimpan data sederhana
+
+        /**
+         * @brief Get the Ability of the card
+         */
+        Ability getAbility();
+
+        /**
+         * @brief Get the description of the card (for example, REROLL: reroll all hand cards)
+         */
+        std::string getDescription();
+
+        // Aktivasi kartu perlu terhubung ke kelas Game untuk mengubah kondisi game
+        // Tidak dibuat setter karena nilai kartu tidak perlu diubah
+};
+
+#endif
