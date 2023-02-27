34c84c2640203694b3c1706d9b5fb400b9bb6e6c
althaafka <althaafka004@gmail.com>
2023-02-27 07:08:43 +0700

    feat: Game Class

---
+++ src/Classes/Game/Game.hpp
@@ -0,0 +1,57 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+using namespace std;
+
+class Game {
+    protected:
+        Player* players;        //pemain
+        int round;              //ronde permainan
+        int giftPoin;           //poin hadiah tiap ronde
+        DeckCard mainDeck;      //deck utama
+        Player currentTurn;     //pemain giliran saat ini
+        int playerTurn[7];      //urutan giliran pemain saat ini
+    public:
+        /*default cctor*/
+        Game();
+        // round 1, giftPoint 64, currentTurn p1, playerTurn (p1,p2,p3,p4,p5,p6,p7)
+
+        /*dtor*/
+        ~Game();
+
+        /*cctor*/
+        Game(const Game&);
+
+        /* Mengembalikan player ke-i*/
+        Player getPlayer(int i);
+
+        /* start game */
+        void start();
+        /* set nickname player
+         * loop untill winning condition
+         *      if (ronde = 1) : set DeckCard
+         *      loop player turn & !winning condition */
+
+        /* winning condition, poin salah satu player >= 2^32*/
+        void isWinning();
+
+        /* mengubah nickname semua player sesuai input */
+        void setPlayers();
+
+        /* inisiasi mainDeck dan membagikan kartu pemain pada awal ronde (user-input/file based)*/
+        void setDeckCard();
+
+        /* mengubah menjadi x kali semula, giftPoin tidak boleh <1*/
+        void multiplyGiftPoin();
+
+        /* pembagian kartu ability pada ronde kedua */
+        void giveAbilityCard();
+
+        /* memberi giftPoin kepada player pemenang ronde */
+        void endOfRound();
+
+        /* mekanisme giliran pemain, currentPlayer akan input command dan command akan dijalankan */
+        void playerTurn();
+};
+
+#endif
