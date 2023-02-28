5fb8da99e4711d65da7fe07e3b66ff4ada92f206
fakihap <fakih.anugerah@gmail.com>
2023-03-01 00:52:19 +0700

    feat : makefile init and random progress on Deck

--- src/Classes/DeckPlayer/DeckPlayer.hpp
+++ src/Classes/DeckPlayer/DeckPlayer.hpp
@@ -4,24 +4,20 @@
 #include "Classes/Deck/Deck.hpp"
 #include "Classes/Card/Card.hpp"
 
-template <typename T>
+template <class T>
 class DeckPlayer: public Deck {
     
     public:
         /**
          * @brief construct new Player Deck with empty array as its attribute
-         * @param size current size of the deck
-         * @param maxSize maximum size of the deck
         */
-        DeckPlayer(int size, int maxSize);
+        DeckPlayer();
 
         /**
          * @brief construct new Player Deck with a given array as its attribute
-         * @param value array that will be set as the attribute of this Deck
-         * @param size current size of the deck
-         * @param maxSize maximum size of the deck
+         * @param vec array that will be set as the attribute of this Deck
         */
-        DeckPlayer(Card<T>* value, int size, int maxSize);
+        DeckPlayer(std::vector<Card<T>> vec);
 
         /**
          * @brief construct new Player Deck based on an already constructed Deck
