dec62c9b54694f2c07d018f8e38a90b83be8a8c8
althaafka <althaafka004@gmail.com>
2023-02-28 19:52:39 +0700

    chore : added command and fixed tamplating for Game classes

--- src/Classes/Game/Game.hpp
+++ src/Classes/Game/Game.hpp
@@ -1,60 +1,136 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include "../PlayerCollection/PlayerCollection.hpp"
+#include "../DeckGame/DeckGame.hpp"
+
+#include <vector>
 using namespace std;
 
 class Game {
     protected:
-        Player* players;        //pemain
-        int round;              //ronde permainan
-        int giftPoin;           //poin hadiah tiap ronde
-        DeckCard mainDeck;      //deck utama
-        Player currentTurn;     //pemain giliran saat ini
-        int playerTurn[7];      //urutan giliran pemain saat ini
+        PlayerCollection players;   //pemain
+        int round;                  //ronde permainan
+        int giftPoint;              //poin hadiah tiap ronde
+        DeckGame mainDeck;          //deck utama
+        Player currentTurn;         //pemain giliran saat ini
+        vector<int> playerTurn;        //urutan giliran pemain saat ini
     public:
-        /*default cctor*/
+        /**
+        * @brief Construct a new Game object.
+        * round 1, giftPoint 64, currentTurn p1, playerTurn [1,2,3,4,5,6,7]
+        */
         Game();
-        // round 1, giftPoint 64, currentTurn p1, playerTurn (p1,p2,p3,p4,p5,p6,p7)
 
-        /*dtor*/
+        /**
+         * @brief Destroy the Game object
+         * 
+         */
         ~Game();
 
-        /*cctor*/
+        /**
+         * @brief Construct a copy of Game object
+         * 
+         */
         Game(const Game&);
 
-        /* Mengembalikan player ke-i*/
-        Player getPlayer(int i);
+        /**
+         * @brief Get the Player object
+         * 
+         * @param playerId
+         * @return Player 
+         */
+        Player getPlayer(int playerId);
 
-        /* start game */
+        /**
+         * @brief start game
+         * 
+         */
         void start();
         /* set nickname player
          * loop untill winning condition
          *      if (ronde = 1) : set DeckCard
          *      loop player turn & !winning condition */
 
-        /* winning condition, poin salah satu player >= 2^32*/
-        void isWinning();
+        /**
+         * @brief winning condition : one of the players has 2^32>= points
+         * 
+         * @return true 
+         * @return false 
+         */
+        bool isWinning();
 
-        /* mengubah nickname semua player sesuai input */
-        void setPlayers();
-
-        /* inisiasi mainDeck dan membagikan kartu pemain pada awal ronde (user-input/file based)*/
+        /**
+         * @brief Set the Deck Card object,
+         * inisiasi mainDeck dan membagikan kartu pemain pada awal ronde (user-input/file based)
+         */
         void setDeckCard();
 
-        /* mengubah menjadi x kali semula, giftPoin tidak boleh <1*/
-        void multiplyGiftPoin();
 
-        /* pembagian kartu ability pada ronde kedua */
-        void giveAbilityCard();
-
-        /* memberi giftPoin kepada player pemenang 6 ronde */
+        /**
+         * @brief give gift point to the player with the highest point 
+         * 
+         */
         void endOfRound6();
 
-        /* mekanisme giliran pemain, currentPlayer akan input command dan command akan dijalankan */
+        /**
+         * @brief mekanisme giliran pemain, currentPlayer akan input command dan command akan dijalankan
+         * 
+         */
         void playerTurn();
-        
-        // blm selesai
 
+        /**
+         * @brief change currentPlayer to the next player in playerTurn
+         * 
+         */
+        void nextTurn();
+
+        /**
+         * @brief change playerTurn at the end of the round
+         * 
+         */
+        void nextRoundPlayerTurn();
+
+        ////not for chapsa////
+        /**
+         * @brief multiply gift point by x
+         * 
+         * @param x gift point multiplier
+         */
+        void multiplyGiftPoin(int x);
+
+        /**
+         * @brief give players ability card in round 2
+         * 
+         */
+        void giveAbilityCard();
+
+        /**
+         * @brief activate ability card that the current player has
+         * 
+         */
+        void activateAbility();
+
+        /**
+         * @brief reverse the rest of playerTurn
+         * 
+         */
+        void reverseTurn();
+
+        void commandDOUBLE();
+        void commandNEXT();
+        void commandHALF();
+
+        void commandREROLL();
+        void commandQUADRUPLE();
+        void commandHALF();
+        void commandQUARTER();
+        void commandREVERSE();
+        void commandSWAPCARD();
+        void commandSWITCH();
+        void commandABILITYLESS();
+
+        // blm selesai
 };
 
 #endif
