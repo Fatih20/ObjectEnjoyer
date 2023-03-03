deac3e6143503d4c1df0afa99690a3274f2fa4e2
fakihap <fakih.anugerah@gmail.com>
2023-03-04 01:57:23 +0700

    feat : construct DeckPlayer with DeckGame

---
+++ src/Classes/DeckPlayer/DeckPlayer.cpp
@@ -0,0 +1,8 @@
+#include "DeckPlayer.hpp"
+
+template <typename T>
+DeckPlayer<T>::DeckPlayer(DeckGame<T>& other) {
+    for(int i = 0; i < 2; i++){ // may go generalizing it into "intended deck size"
+        this->vec.insert(other.drawCard());
+    }
+}
--- src/Classes/DeckPlayer/DeckPlayer.hpp
+++ src/Classes/DeckPlayer/DeckPlayer.hpp
@@ -2,34 +2,82 @@
 #define _DECKPLAYER_HPP_
 
 #include "Classes/Deck/Deck.hpp"
+#include "Classes/DeckGame/DeckGame.hpp"
+
 #include "Classes/Card/Card.hpp"
 
 template <class T>
-class DeckPlayer: public Deck {
+class DeckPlayer: public Deck<T> {
     
     public:
-        /**
-         * @brief construct new Player Deck with empty array as its attribute
-        */
-        DeckPlayer();
+        // /**
+        //  * @brief construct new Player Deck with empty array as its attribute
+        // */
+        // DeckPlayer();
+
+        // /**
+        //  * @brief construct new Player Deck with a given array as its attribute
+        //  * @param vec array that will be set as the attribute of this Deck
+        // */
+        // DeckPlayer(std::vector<Card<T>> vec);
+
+        // /**
+        //  * @brief construct new Player Deck based on an already constructed Deck
+        //  * 
+        //  * @param other the other Deck
+        // */
+        // DeckPlayer(const DeckPlayer& other);
+
+        // /**
+        //  * @brief destruct constructed Player Deck
+        // */
+        // ~DeckPlayer();
 
         /**
-         * @brief construct new Player Deck with a given array as its attribute
-         * @param vec array that will be set as the attribute of this Deck
+         * @brief construct a DeckPlayer whose content is made of cards drawed from DeckGame
         */
-        DeckPlayer(std::vector<Card<T>> vec);
+       DeckPlayer(DeckGame<T>&);
 
         /**
-         * @brief construct new Player Deck based on an already constructed Deck
+         * @brief compare the combination value of two Decks
          * 
-         * @param other the other Deck
+         * @return true Left player has worse combination
+         * @return false Left player has better combination
         */
-        DeckPlayer(const DeckPlayer& other);
+        bool operator<(const DeckPlayer&);
 
         /**
-         * @brief destruct constructed Player Deck
+         * @brief compare the combination value of two Decks
+         * 
+         * @return true Left player has better combination
+         * @return false Left player has worse combination
         */
-        ~DeckPlayer();
+        bool operator>(const DeckPlayer&);
+
+        /**
+         * @brief compare the combination value of two Decks
+         * 
+         * @return true Both players have the same combination value
+         * @return false The two players have different value of cards combination
+        */
+        bool operator==(const DeckPlayer&);
+
+        /**
+         * @brief compare the combination value of two Decks
+         * 
+         * @return true Left player has worse if not the same value of cards combination
+         * @return false Left player has better combination
+        */
+        bool operator<=(const DeckPlayer&);
+
+        /**
+         * @brief compare the combination value of two Decks
+         * 
+         * @return true Left player has better if not the same value of cards combination
+         * @return false Left player has worse combination
+        */
+        bool operator>=(const DeckPlayer&);
+    
 };
 
 #endif
