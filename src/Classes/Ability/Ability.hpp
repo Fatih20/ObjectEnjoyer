640df7849b14da5178c98f868d9d37450ca77513
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-03-04 02:40:49 +0700

    feat: made ability into an object

---
+++ src/Classes/Ability/Ability.hpp
@@ -0,0 +1,58 @@
+#ifndef _ABILITY_HPP_
+#define _ABILITY_HPP_
+
+#include <iostream>
+#include <map>
+
+typedef enum{
+    REROLL, // Reroll all hand cards
+    QUADRUPLE, // Quadruple the reward of the current round
+    QUARTER, // Quarter the reward of the current round
+    REVERSE, // Reverse the order of the players
+    SWAP, // Swap a card from the hand with a card from another player's hand
+    SWITCH, // Switch the hand cards with another player's hand cards
+    ABILITYLESS // Disable the ability of a specific player
+} Abi;
+
+// nanti pindahkan ke cpp
+extern const std::map<Abi, std::string> abilityDescription = {
+    {REROLL, "Reroll all hand cards"},
+    {QUADRUPLE, "Quadruple the reward of the current round"},
+    {QUARTER, "Quarter the reward of the current round"},
+    {REVERSE, "Reverse the order of the players"},
+    {SWAP, "Swap a card from the hand with a card from another player's hand"},
+    {SWITCH, "Switch the hand cards with another player's hand cards"},
+    {ABILITYLESS, "Disable the ability of a specific player"}
+};
+
+class Ability {
+    private:
+        Abi ability;
+    
+    public:
+        /**
+         * @brief Construct a new Ability object with given ability
+         * 
+         * @param ability The ability of the card (REROLL, QUADRUPLE, QUARTER, REVERSE, SWAP, SWITCH, ABILITYLESS)
+         */
+        Ability(Abi ability);
+
+        /**
+         * @brief Get the Ability of the card from integer conversion to enum
+         * 
+         */
+        Ability getAbility();
+
+        /**
+         * @brief Get the description of the card (for example, REROLL: reroll all hand cards)
+         */
+        std::string getDescription();
+
+        /**
+         * @brief Activate the ability
+         * 
+         */
+        virtual void activateAbility() = 0;
+};
+
+#endif
