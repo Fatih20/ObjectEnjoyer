bb5106b3d5533a956afb99b5dce2f3c1c2702b65
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-03-12 09:59:55 +0700

    feat: isOnePair checker

--- src/Classes/Combination/Combination.cpp
+++ src/Classes/Combination/Combination.cpp
@@ -1,12 +1,76 @@
 #include "Combination.hpp"
+#include <algorithm>
+#include <iostream>
 
-bool isStraightFlush(Deck<int> player, Deck<int> table){
-    
+Combination::Combination(vector<ColorCard> player, vector<ColorCard> table){
+    playerCards = player;
+    allCards = player;
+    allCards.insert(allCards.end(), table.begin(), table.end());
+    sort(allCards.begin(), allCards.end(), [](ColorCard a, ColorCard b){
+        return a.value() > b.value();
+    });
+    // calculate(player, table);
 }
 
-int Combination::calculate(Deck<int> player, Deck<int> table){
+// void Combination::calculate(vector<ColorCard> player, vector<ColorCard> table){
+//     if(isStraightFlush(player, table)){
+//         combinationType = STRAIGHT_FLUSH;
+//     }
+//     else if(isFourOfAKind(player, table)){
+//         combinationType = FOUR_OF_A_KIND;
+//     }
+//     else if(isFullHouse(player, table)){
+//         combinationType = FULL_HOUSE;
+//     }
+//     else if(isFlush(player, table)){
+//         combinationType = FLUSH;
+//     }
+//     else if(isStraight(player, table)){
+//         combinationType = STRAIGHT;
+//     }
+//     else if(isThreeOfAKind(player, table)){
+//         combinationType = THREE_OF_A_KIND;
+//     }
+//     else if(isTwoPair(player, table)){
+//         combinationType = TWO_PAIRS;
+//     }
+//     else if(isOnePair(player, table)){
+//         combinationType = PAIR;
+//     }
+//     else{
+//         combinationType = HIGH_CARD;
+//         getHighestCard(player, table);
+//     }
+// }
 
+bool Combination::inPlayer(){
+    for(int i = 0; i < usedCards.size(); i++){
+        if(find(playerCards.begin(), playerCards.end(), usedCards[i]) != playerCards.end()){
+            return true;
+        }
+    }
+    return false;
 }
+
+bool Combination::isOnePair(vector<ColorCard> player, vector<ColorCard> table){
+    this->usedCards.clear();
+    for(int i = 0; i < allCards.size(); i++){
+        for(int j = i + 1; j < allCards.size(); j++){
+            if(allCards[i].getNumber() == allCards[j].getNumber()){
+                this->usedCards.push_back(allCards[i]);
+                this->usedCards.push_back(allCards[j]);
+                if(inPlayer()){
+                    return true;
+                } else {
+                    usedCards.clear();
+                }
+            }
+        }
+    }
+    return false;
+}
+
+
 
 // buat dengan array of cards dulu, baru diubah ke deck nanti ketika dack udah jadi
 
@@ -18,3 +82,43 @@ boleh selama:
 4. ga ngilangin konsep oop
 5. ada subtitusi penggunaan oop, jangan sampe gegara implementasi sendiri, misal polymorphismnya ilang, ini gada toleransi
 */
+
+void Combination::print(){
+    switch(combinationType){
+        case HIGH_CARD:
+            cout << "High Card" << endl;
+            break;
+        case PAIR:
+            cout << "Pair" << endl;
+            break;
+        case TWO_PAIRS:
+            cout << "Two Pairs" << endl;
+            break;
+        case THREE_OF_A_KIND:
+            cout << "Three of a Kind" << endl;
+            break;
+        case STRAIGHT:
+            cout << "Straight" << endl;
+            break;
+        case FLUSH:
+            cout << "Flush" << endl;
+            break;
+        case FULL_HOUSE:
+            cout << "Full House" << endl;
+            break;
+        case FOUR_OF_A_KIND:
+            cout << "Four of a Kind" << endl;
+            break;
+        case STRAIGHT_FLUSH:
+            cout << "Straight Flush" << endl;
+            break;
+    }
+    for(int i = 0; i < usedCards.size(); i++){
+        usedCards[i].print();
+    }
+    cout<<"Score: "<<score<<endl;
+    cout<<"All Cards: "<<endl;
+    for(int i = 0; i < allCards.size(); i++){
+        allCards[i].print();
+    }
+}
--- src/Classes/Combination/Combination.hpp
+++ src/Classes/Combination/Combination.hpp
@@ -2,6 +2,9 @@
 #define _COMBINATION_HPP_
 
 #include "../Deck/Deck.hpp"
+#include "../ColorCard/ColorCard.hpp"
+#include <vector>
+using namespace std;
 
 class Combination{
     private:
@@ -16,33 +19,37 @@ class Combination{
             FOUR_OF_A_KIND,
             STRAIGHT_FLUSH,
         } CombinationType;
+        int score;
+        CombinationType combinationType;
+        vector <ColorCard> allCards;
+        vector <ColorCard> playerCards;
+        vector <ColorCard> usedCards;
     public:
-        /**
-         * @brief Calculate the score of the player's hand
-         * 
-         * @param player The player's hand
-         * @param table The table's cards
-         * @return int The score of the player's hand
-         */
-        static int calculate(Deck<int> player, Deck<int> table);
+        Combination(vector<ColorCard> player, vector<ColorCard> table);
 
-        bool isStraightFlush(Deck<int> player, Deck<int> table);
+        // void calculate(vector<ColorCard> player, vector<ColorCard> table);
 
-        bool isFourOfAKind(Deck<int> player, Deck<int> table);
+        // bool isStraightFlush(vector<ColorCard> player, vector<ColorCard> table);
 
-        bool isFullHouse(Deck<int> player, Deck<int> table);
+        // bool isFourOfAKind(vector<ColorCard> player, vector<ColorCard> table);
 
-        bool isFlush(Deck<int> player, Deck<int> table);
+        // bool isFullHouse(vector<ColorCard> player, vector<ColorCard> table);
 
-        bool isStraight(Deck<int> player, Deck<int> table);
+        // bool isFlush(vector<ColorCard> player, vector<ColorCard> table);
 
-        bool isThreeOfAKind(Deck<int> player, Deck<int> table);
+        // bool isStraight(vector<ColorCard> player, vector<ColorCard> table);
 
-        bool isTwoPairs(Deck<int> player, Deck<int> table);
+        // bool isThreeOfAKind(vector<ColorCard> player, vector<ColorCard> table);
 
-        bool isPair(Deck<int> player, Deck<int> table);
+        bool isTwoPair(vector<ColorCard> player, vector<ColorCard> table);
 
-        bool isHighestCombo(Deck<int> table);
+        bool isOnePair(vector<ColorCard> player, vector<ColorCard> table);
+
+        void getHighestCard(vector<ColorCard> player, vector<ColorCard> table);
+
+        bool inPlayer();
+
+        void print();
 };
 
 #endif
