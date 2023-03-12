9d4b9962cc62e8a46174ceea495b564b8d8262f2
Fatih20 <fnri39@protonmail.com>
2023-03-12 21:22:26 +0700

    Dodging error by disabling main in game.

--- src/Classes/Game/main.cpp
+++ src/Classes/Game/main.cpp
@@ -1,23 +1,23 @@
-#include "../GameCandy/GameCandy.hpp"
-#include "../GameCapsa/GameCapsa.hpp"
-#include "Game.hpp"
-#include "../GameCandy/GameCandyException/GameCandyException.hpp"
-#include <iostream>
+// #include "../GameCandy/GameCandy.hpp"
+// #include "../GameCapsa/GameCapsa.hpp"
+// #include "Game.hpp"
+// #include "../GameCandy/GameCandyException/GameCandyException.hpp"
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-int main(){
-    Game *game;
-    int opt;
-    cout << "Game apah?\n";
-    cout << "1. Candy\n";
-    cout << "2. Capsa\n";
-    cin >> opt;
-    if (opt==1){
-        game = new GameCandy;
-    } else {
-        game = new GameCapsa;
-    }
-    game->start();
-    return 0;
-}
+// int main(){
+//     Game *game;
+//     int opt;
+//     cout << "Game apah?\n";
+//     cout << "1. Candy\n";
+//     cout << "2. Capsa\n";
+//     cin >> opt;
+//     if (opt==1){
+//         game = new GameCandy;
+//     } else {
+//         game = new GameCapsa;
+//     }
+//     game->start();
+//     return 0;
+// }
