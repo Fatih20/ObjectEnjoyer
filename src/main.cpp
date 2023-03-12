138d066e2322799c735685fe5631effbd153939a
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-03-12 19:38:05 +0700

    chore: main body for testing combination

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
-#include "Classes/Player/Player.hpp"
+#include "Classes/Combination/Combination.hpp"
+#include <iostream>
+using namespace std;
 
-int main()
-{
-
-    Player p5;
-    p5 += 10;
-
-    p5.printScore();
+int main(){
+    vector<ColorCard> player;
+    vector<ColorCard> table;
+    for(int i = 0; i < 2; i++){
+        int value;
+        int color;
+        cin >> value >> color;
+        player.push_back(ColorCard(value, Color(color)));
+    }
+    for(int i = 0; i < 5; i++){
+        int value;
+        int color;
+        cin >> value >> color;
+        table.push_back(ColorCard(value, Color(color)));
+    }
+    Combination comb(player, table);
+    comb.print();
 }
