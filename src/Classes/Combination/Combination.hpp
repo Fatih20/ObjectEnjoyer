760044d4eb522b7d8f896c89dc7c645e41b73602
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-03-14 04:44:15 +0700

    chore: doxygen documenting

--- src/Classes/Combination/Combination.hpp
+++ src/Classes/Combination/Combination.hpp
@@ -27,36 +27,144 @@ class Combination{
                 Deck<ColorCard> cards; // cards used to make the combination
                 ColorCard highestCard; // highest card in the combination
 
+                /**
+                 * @brief Convert the combination type to a string
+                 * 
+                 */
                 std::string getTypeString();
 
+                /**
+                 * @brief Remove the cards that are not needed to make the combination
+                 * 
+                 */
                 void reduceCards();
 
+                /**
+                 * @brief Check if the combination is a pair
+                 * 
+                 */
                 bool isPair();
+
+                /**
+                 * @brief Check if the combination is a two pairs
+                 * 
+                 */
                 bool isTwoPairs();
+
+                /**
+                 * @brief Check if the combination is a three of a kind
+                 * 
+                 */
                 bool isThreeOfAKind();
+
+                /**
+                 * @brief Check if the combination is a straight
+                 * 
+                 */
                 bool isStraight();
+
+                /**
+                 * @brief Check if the combination is a flush
+                 * 
+                 */
                 bool isFlush();
+
+                /**
+                 * @brief Check if the combination is a full house
+                 *  
+                 */
                 bool isFullHouse();
+
+                /**
+                 * @brief Check if the combination is a four of a kind
+                 * 
+                 */
                 bool isFourOfAKind();
+
+                /**
+                 * @brief Check if the combination is a straight flush
+                 * 
+                 */
                 bool isStraightFlush();
 
             public:
+                /**
+                 * @brief Construct a new Combo object with one dummy card
+                 * 
+                 */
                 Combo();
+
+                /**
+                 * @brief Construct a new Combo object with the given cards
+                 * 
+                 */
                 Combo(Deck<ColorCard> cards);
 
+                /**
+                 * @brief Get the Highest Card Played in the combination
+                 * 
+                 * @return ColorCard 
+                 */
                 ColorCard getHighestCard();
+
+                /**
+                 * @brief Get all the cards used to make the combination
+                 * 
+                 */
                 Deck<ColorCard> getCards();
+
+                /**
+                 * @brief Get the type of the combination
+                 * 
+                 */
                 CombinationType getType();
 
+                /**
+                 * @brief Check if the combination uses at least one of the cards from the given deck
+                 *  
+                 */
                 bool useOneOf(Deck<ColorCard> cards);
+
+                /**
+                 * @brief Get the Highest Card ni the intersection between the cards used to make the combination and the given cards
+                 * 
+                 */
                 ColorCard getHighestCardIntersection(Deck<ColorCard> cards);
 
+                /**
+                 * @brief Print the combination
+                 * 
+                 */
                 void print();
 
+                /**
+                 * @brief Overload the < operator to compare two combinations using the rules of poker
+                 * 
+                 */
                 bool operator<(Combo rhs);
+
+                /**
+                 * @brief Overload the <= operator to compare two combinations using the rules of poker
+                 * 
+                 */
                 bool operator<=(Combo rhs);
+
+                /**
+                 * @brief Overload the > operator to compare two combinations using the rules of poker
+                 * 
+                 */
                 bool operator>(Combo rhs);
+
+                /**
+                 * @brief Overload the >= operator to compare two combinations using the rules of poker
+                 * 
+                 */
                 bool operator>=(Combo rhs);
+
+                /**
+                 * @brief Overload the == operator to compare two combinations using the rules of poker
+                 * 
+                 */
                 bool operator==(Combo rhs);
         };
 
@@ -64,25 +172,67 @@ class Combination{
         Deck<ColorCard> allCards; // all cards played for this combination
         Deck<ColorCard> playerCards; // cards played by the player
 
+        /**
+         * @brief Get the Best Combo possible from all permutation of the given cards
+         * 
+         */
         Combo getBestCombo();
 
+        /**
+         * @brief Get the Highest Card Played by the player in the combination
+         * 
+         */
         ColorCard getHighestPlayerCard();
     
     public:
+        /**
+         * @brief Construct a new Combination object with one deck
+         * 
+         */
         Combination(Deck<ColorCard> cards);
 
+        /**
+         * @brief Construct a new Combination object with two decks
+         * 
+         * @param player Cards played by the player
+         * @param table Cards played on the table
+         */
         Combination(Deck<ColorCard> player, Deck<ColorCard> table);
 
+        /**
+         * @brief Print the combination details
+         * 
+         */
         void print();
 
+        /**
+         * @brief Overload the < operator to compare two combinations using the rules of poker
+         * 
+         */
         bool operator<(Combination rhs);
 
+        /**
+         * @brief Overload the <= operator to compare two combinations using the rules of poker
+         * 
+         */
         bool operator<=(Combination rhs);
 
+        /**
+         * @brief Overload the > operator to compare two combinations using the rules of poker
+         * 
+         */
         bool operator>(Combination rhs);
 
+        /**
+         * @brief Overload the >= operator to compare two combinations using the rules of poker
+         * 
+         */
         bool operator>=(Combination rhs);
 
+        /**
+         * @brief Overload the == operator to compare two combinations using the rules of poker
+         * 
+         */
         bool operator==(Combination rhs);
 };
 
