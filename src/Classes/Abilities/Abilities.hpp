cde74f813fac247222ab9417ea225eb9061d5e68
Fatih20 <fnri39@protonmail.com>
2023-03-14 10:22:55 +0700

    Fix untracked.

---
+++ src/Classes/Abilities/Abilities.hpp
@@ -0,0 +1,16 @@
+
+class GameCandy;
+
+class Quadruple : public Ability
+{
+public:
+    Quadruple();
+    void activateAbility(GameCandy &gC);
+};
+
+class Quarter : public Ability
+{
+public:
+    Quarter();
+    void activateAbility(GameCandy &gC);
+};
