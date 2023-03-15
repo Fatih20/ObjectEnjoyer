61a8d5f0026815b32fb72548e64a004e4ed76f4a
althaafka <althaafka004@gmail.com>
2023-03-15 15:53:45 +0700

    feat : add useabilityless

--- src/Classes/GameCandy/GameCandy.cpp
+++ src/Classes/GameCandy/GameCandy.cpp
@@ -433,13 +433,19 @@ void GameCandy::abilitylessAbility()
     bool disableUseful = players.disablePlayerAbility(properIndex);
     if (disableUseful)
     {
-        cout << "Kartu ability player " << properIndex << " telah dimatikan.\n";
+        cout << "Kartu ability player " << players.getNthPlayer(properIndex) << " telah dimatikan.\n";
     } 
+    else if (players.getTurns().at(6) == players.getPlayerWithTurn().getGameID()){
+        cout << "Eits, ternyata semua pemain sudah memakai kartu kemampuan. \n";
+        cout << "Yah kamu kena sendiri deh, kemampuanmu menjadi abilityless. \n";
+        cout << "Yah, pengunaan kartu ini sia-sia\n";
+    }
     else
     {
-        cout << "Kartu ability player " << properIndex << " telah dipakai sebelumnya.\n";
+        cout << "Kartu ability player " << players.getNthPlayer(properIndex) << " telah dipakai sebelumnya.\n";
         cout << "Yah, sayang penggunaan kartu ini sia-sia\n";
     }
+
 }
 
 void GameCandy::reverseAbility()
