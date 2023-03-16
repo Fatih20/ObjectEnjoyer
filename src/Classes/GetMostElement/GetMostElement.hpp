66f9c251aff17c545e152465286e889bb191dc83
Fatih20 <fnri39@protonmail.com>
2023-03-16 21:05:24 +0700

    Bug fix in GetMostElement.

--- src/Classes/GetMostElement/GetMostElement.hpp
+++ src/Classes/GetMostElement/GetMostElement.hpp
@@ -6,17 +6,11 @@ using namespace std;
 template <typename T>
 class GetMostElement
 {
-<<<<<<< HEAD
-    public:
-        static T getHighest(T[]);
-=======
 public:
     static T getHighest(T[]);
->>>>>>> 735b696bdd00ffb40d84928d32d744fa88401aea
+    static T getLowest(T[]);
 
-        static T getLowest(T[]);
+    static T getHighest(vector<T>);
 
-        static T getHighest(vector<T>);
-
-        static T getLowest(vector<T>);
+    static T getLowest(vector<T>);
 };
